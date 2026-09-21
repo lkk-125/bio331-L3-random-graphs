e54e822477fc7dec0e004a3518ff54ef0e6af36b
lkk-125 <kubocm@reed.edu>
2026-09-21 22:47:58 +0000

    implemented functions to generate graphs using the Erdös-Rényi and Barabási-Albert models, with beginnings of the Watts-Strogatz model

--- lab3.py
+++ lab3.py
@@ -2,21 +2,78 @@
 import random
 from pyvis.network import Network
 
+random.seed(19940913)
+
 def main():
     """
-    Main function. Takes not inputs and returns nothing.
+    Main function. Takes no inputs and returns nothing.
     """
     ## CALL your functions here.
-
-
+    test = []
+    test.append(gen_ER(25, 100))
+    test.append(gen_BA(3, 1, 100))
+    test.append(gen_BA(3, 2, 50))
+    for i in range(len(test)):
+        viz_graph(test[i], "test" + str(i) + ".html")
+    
     return
 
 ## WRITE your own function definitions here.
 
+def gen_ER(n, m):
+    nodes = []
+    edges = []
+    for i in range(n):
+        nodes.append(str(i+1))
+    while len(edges) < m:
+        u = random.choice(nodes)
+        v = u
+        while v == u:
+            v = random.choice(nodes)
+        if not ([u, v] in edges or [v, u] in edges):
+            edges.append([u, v])
+    return edges 
+
+def gen_BA(n, m, t):
+    nodes = ['0']
+    edges = []
+    for i in range(n-1):
+        nodes.append(str(i+1))
+        edges.append([str(i), str(i+1)])
+    for i in range(t):
+        bag = []
+        for node in nodes: #this for loop calculates degree - getDeg
+            d = 0
+            for edge in edges:
+                if node in edge:  
+                    d += 1
+            for j in range(d): # this loop creates a "bag" of node duplicates with proportions equal to their degree      
+                bag.append(node)              
+        temp = len(nodes)
+        v = str(temp)
+        nodes.append(v)
+        for j in range(m): 
+            u = random.choice(bag)
+            while ([u, v] in edges or [v, u] in edges):
+                u = random.choice(bag)
+            edges.append([u, v])
+    return edges  
+
+def gen_WS(n, k, pr):
+    nodes = ['0']
+    edges = []
+
+    avdeg = 2*round(k/2) #ensures k is even against user error
+
+    #initial graph
+    for i in range(n-1): #make a line of nodes of length n
+        nodes.append(str(i+1))
+        edges.append([str(i), str(i+1)])
+    if n != 2: #close the loop
+        edges.append(['0', str(n-1)])
 
 
-
-def viz_graph(nodes,edges,outfile):
+def viz_graph(edges,outfile):
     """
     Visualize a graph and write it to an HTML file.
     :param: nodes - list or set of nodes
@@ -27,10 +84,24 @@ def viz_graph(nodes,edges,outfile):
     
     # Refer to Lab 1 for instructions about visualizing a graph.
     G = Network() # create graph
-    for n in nodes: # add nodes
-        G.add_node(n,label=str(n),color='#ace3a8',shape='diamond')
-    for u,v in edges: # add edges
-        G.add_edge(u,v) 
+    nodes = []
+    i = 0
+    for u,v in edges: # add nodes an edges per edge without duplicates
+        print("bread")
+        print([u,v])
+        if u not in nodes:
+            print("loaf")
+            print(u)
+            nodes.append(u)
+            G.add_node(u,label=u,color='#ace3a8',shape='diamond')
+        if v not in nodes:
+            print("loaf")
+            print(v)
+            nodes.append(v)
+            G.add_node(v,label=v,color='#ace3a8',shape='diamond')
+        i += 1
+        G.add_edge(u,v,title="Order: " + str(i))
+    print(nodes)
 
     G.toggle_physics(True) 
     G.show_buttons(filter_=['physics'])
